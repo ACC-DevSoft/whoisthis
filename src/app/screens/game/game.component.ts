52c32e2a98986e4d0525048b86f938f11a76e867
AlejaB-droid <alejandra.barrios.1900@gmail.com>
2021-07-02 14:44:34 -0500

    updated on 2-7-21 14:44

--- src/app/screens/game/game.component.ts
+++ src/app/screens/game/game.component.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-import { ServiceService } from '../../services/service.service';
-
-=======
 import { Router } from "@angular/router";
->>>>>>> main
+
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
@@ -15,25 +11,13 @@ export class GameComponent implements OnInit {
   public name: String;
   public score: any;
 
-<<<<<<< HEAD
-  public character: any[];
 
-  constructor(public service: ServiceService) {
-    this.character = [];
+  constructor() {
+    this.name = '';
    }
-=======
-  constructor(private router: Router) { 
-    this.name = 'David';
-    this.score = 0;
-  }
->>>>>>> main
 
   ngOnInit(): void {
-    this.service.getCharacters().subscribe(
-      (res:any) => {
-        this.character = res;
-      }
-    )
+
   }
   goOut(){
     // if (comfirm(this.name + " Do you want finish the game ?")) {
