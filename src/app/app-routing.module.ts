ec006256b30d8ae4db43a04d095c2baa0fe2ddbc
Villadiego Hoyos <d.villadiego.hoyos@accenture.com>
2021-07-02 12:18:15 -0500

    d

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {GameComponent} from './screens/game/game.component';
+import { GameComponent } from './screens/game/game.component';
+import { HomeComponent } from './screens/home/home.component';
+import { LoseComponent } from './screens/lose/lose.component';
+import { WinComponent } from './screens/win/win.component';
+ 
+const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: HomeComponent,
+    },
+    {
+        path: 'game',
+        component: GameComponent
+    },
+    {
+        path: 'lose',
+        component: LoseComponent,
+    },
+    {
+        path: 'win',
+        component: WinComponent,
+    }
+]
 
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
 export class AppRoutingModule { }
