e39ac5521f5e288186341e51646abd9806e1d1c4
Davidhzk2 <dherrera035@misena.edu.co>
2021-07-02 13:27:04 -0500

    navbar style, in game component

--- src/app/screens/game/game.component.ts
+++ src/app/screens/game/game.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from "@angular/router";
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
@@ -7,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class GameComponent implements OnInit {
+  public name: String;
+  public score: any;
 
-  constructor() { }
+  constructor(private router: Router) { 
+    this.name = 'David';
+    this.score = 0;
+  }
 
   ngOnInit(): void {
+  }
+  goOut(){
+    // if (comfirm(this.name + " Do you want finish the game ?")) {
+
+    // }
   }
 
 }
