60c11c0a6021e39df5ac437965861504ea442665
Jalexss <jhalx2198@gmail.com>
2021-07-03 00:00:28 -0500

    Fixed array pjs and installed underscorejs

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServiceService } from '../../services/service.service';
+import * as _ from 'underscore';
 
 @Component({
   selector: 'app-cards',
@@ -9,27 +10,48 @@ import { ServiceService } from '../../services/service.service';
 export class CardsComponent implements OnInit {
 
   public characters: any[];
+  public names: any[];
   public idPhoto: number;
   public idName: number;
   public flag: boolean;
+  public score: number;
+  public restart: boolean;
 
 
   constructor(public service: ServiceService) {
     this.characters = [];
+    this.names = [];
     this.idPhoto = 0;
     this.idName = 0;
+    this.score = 0;
     this.flag = false;
+    this.restart = false;
 
   }
 
   ngOnInit(): void {
+
+    this.starGame();
+
+  }
+
+  starGame() {
     this.service.getCharacters().subscribe(
       (res: any) => {
-        this.characters = res;
-        console.log(this.characters);
+        this.names = res;
+        this.characters = _.shuffle(this.names);
 
       }
     )
+
+  }
+
+  restarGame() {
+    this.idPhoto = 0;
+    this.idName = 0;
+    this.score = 0;
+    this.flag = false;
+    this.starGame();
   }
 
   flipCardPhoto(idx: number) {
@@ -44,16 +66,33 @@ export class CardsComponent implements OnInit {
     this.idName = idx;
     this.flag = false;
 
+    this.check();
+
   }
 
   check() {
 
     if (this.idPhoto === this.idName) {
       console.log('CONGRATULATIONS!!');
+      setTimeout(() => {
+        this.characters = this.characters.filter(chracter => chracter.id !== this.idPhoto);
+        this.names = this.names.filter(name => name.id !== this.idName);
 
-    }else{
+      }, 1000);
+
+      console.log('Lenght is :', this.characters.length);
+
+
+      if (this.characters.length === 1) {
+        this.restart = true;
+      } else {
+        this.restart = false;
+      }
+
+
+
+    } else {
       console.log('SORRY');
-      
     }
 
   }
