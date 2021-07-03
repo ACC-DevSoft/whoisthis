93b585c52e43f5a50a2182abff8e86c3d143ffa2
Jalexss <jhalx2198@gmail.com>
2021-07-02 21:35:31 -0500

    Fixed flip card

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -9,9 +9,17 @@ import { ServiceService } from '../../services/service.service';
 export class CardsComponent implements OnInit {
 
   public characters: any[];
+  public idPhoto: number;
+  public idName: number;
+  public flag: boolean;
+
 
   constructor(public service: ServiceService) {
     this.characters = [];
+    this.idPhoto = 0;
+    this.idName = 0;
+    this.flag = false;
+
   }
 
   ngOnInit(): void {
@@ -19,9 +27,35 @@ export class CardsComponent implements OnInit {
       (res: any) => {
         this.characters = res;
         console.log(this.characters);
-        
+
       }
     )
+  }
+
+  flipCardPhoto(idx: number) {
+    console.log(idx);
+    this.idPhoto = idx;
+    this.flag = true
+
+  }
+
+  flipCardName(idx: number) {
+    console.log(idx);
+    this.idName = idx;
+    this.flag = false;
+
+  }
+
+  check() {
+
+    if (this.idPhoto === this.idName) {
+      console.log('CONGRATULATIONS!!');
+
+    }else{
+      console.log('SORRY');
+      
+    }
+
   }
 
 }
